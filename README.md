586531e5e28d4a6a9229a8291248d569d2ebe6c9
Ignacio R. Galieri <ignacio.galieri@intraway.com>
2016-03-01 00:16:18 -0500

    Readme And Licence

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# websocket-monitoring
-This an example of integration between BE and FE using WebSocket.
+Websocket Monitoring
+====================
+
+This an example of integration between BE and FE using WebSocket. [Live Demo](http://elnachonerd.com:8080)
+
+Usage
+-----
+
+```bash
+$> git clone git@github.com:irgalieri/websocket-monitoring.git
+$> bower install
+$> npm install
+$> npm start
+```
+
+The open your bowser http://localhost:8080/
+
+License
+-------
+
+The HTML Template was created by Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-sb-admin-2/blob/gh-pages/LICENSE) license. Code is released under the GPL-3.0 license. See the LICENSE for more information.
--- wsserver.js
+++ wsserver.js
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
+/**
+ * WebSocket Monitoring
+ * Copyright (C) 2016  Ignacio R. Galieri
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   Library
+ * @package    WsServer
+ * @author     Ignacio R. Galieri <irgalieri@gmail.com>
+ * @copyright  2016 Ignacio R. Galieri
+ * @license    GPL-3.0
+ * @link       https://github.com/irgalieri/websocket-monitoring
+ */
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 var os = require('os');
